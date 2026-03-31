d3a76002da4d44848e30f74ecb6669908915734a
codebuffdev <codebuffdev@gmail.com>
2026-03-31 17:46:44 +0530

    feat: addling live api & cors proxy urls

--- 06-exploring-the-world/src/utils/constant.js
+++ 06-exploring-the-world/src/utils/constant.js
@@ -2,3 +2,8 @@ export const APP_LOGO_URL =
     "https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=612x612&w=0&k=20&c=HXPxcjOxUiW4pMW1u9E0k2dJYQOU37a_0qZAy3so8fY=";
 
 export const CART_IMG = "https://cdn-icons-png.flaticon.com/512/1413/1413908.png"
+
+export const cloudinaryImageLink = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const corsProxy = "https://corsproxy.io/?"
+export const liveApi = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.84730&lng=84.0074&page_type=DESKTOP_WEB_LISTING";
